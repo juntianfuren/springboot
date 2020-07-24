8256d048019000cf30a0b8a5988cff6aa30150df
chenkangming <chenkangming@xsyxsc.com>
2020-07-24 15:07:07 +0800

    提交代码

---
+++ spring-rocketmq/src/main/java/com/jtfr/producer/Demo03Producer.java
@@ -0,0 +1,37 @@
+package com.jtfr.producer;
+// Demo03Producer.java
+
+import org.apache.rocketmq.client.producer.SendCallback;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+import com.jtfr.message.Demo03Message;
+
+@Component
+public class Demo03Producer {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
+
+    public SendResult syncSendDelay(Integer id, int delayLevel) {
+        // 创建 Demo03Message 消息
+        Message message = MessageBuilder.withPayload(new Demo03Message().setId(id))
+                .build();
+        // 同步发送消息
+        return rocketMQTemplate.syncSend(Demo03Message.TOPIC, message, 30 * 1000,
+                delayLevel);
+    }
+
+    public void asyncSendDelay(Integer id, int delayLevel, SendCallback callback) {
+        // 创建 Demo03Message 消息
+        Message message = MessageBuilder.withPayload(new Demo03Message().setId(id))
+                .build();
+        // 同步发送消息
+        rocketMQTemplate.asyncSend(Demo03Message.TOPIC, message, callback, 30 * 1000,
+                delayLevel);
+    }
+
+}
